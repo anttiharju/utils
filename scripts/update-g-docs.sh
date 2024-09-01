8988866d65c7fb283b88489328ba33f0254f0440
anttiharju <git@anttiharju.dev>
2024-09-01 11:05:54 +0300

    Enforce up-to-date docs via GitHub Actions

---
+++ scripts/update-g-docs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+if ! git diff --quiet --staged; then
+	echo "You have staged changes; cannot ensure docs are up-to-date."
+	exit 1
+fi
+
+./scripts/g.sh
+
+if git diff --quiet -- bin/g; then
+	exit 0
+fi
+
+# Set source to empty string if $1 is unbound.
+source="${1:-}"
+
+git add bin/g
+name="github-actions[bot]"
+email="41898282+github-actions[bot]@users.noreply.github.com"
+GIT_COMMITTER_NAME="$name" GIT_COMMITTER_EMAIL="$email" git commit --no-verify -m \
+"Update bin/g docs$source"
