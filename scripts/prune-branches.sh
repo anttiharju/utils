b56ae9532dd00162495cb2326fafed08201e811a
Antti Harju <antti.harju@smartly.io>
2025-03-31 09:12:31 +0300

    Retain default branch

--- scripts/prune-branches.sh
+++ scripts/prune-branches.sh
@@ -4,4 +4,4 @@ set -eu
 # prune-branches
 # delete all local branches except the current one
 
-git branch | grep -v "^\*" | xargs git branch -D
+git branch | grep -v "^\*\|main\|master" | xargs git branch -D
